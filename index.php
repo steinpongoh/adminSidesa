0b44d433d3cbe5f870484fdbf160c613078b73d7
Stein Pongoh <steinpongoh@gmail.com>
2023-10-07 18:00:25 +0800

    awal

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include 'functions/functions.php';
+include 'templates/header.php';
+
+$pageName='Dashboard';
+?>
+
+
+<?php include 'templates/footer.php'?>
+
+
---
+++ templates/footer.php
@@ -0,0 +1,7 @@
+    <script src="/adminSidesa/scripts/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+</body>
+
+</html> 
